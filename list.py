94fa29968d211dc1cf0eb59cada3d4b7d33372d1
Frkanimall <unuma240414@gmail.com>
2023-12-21 21:00:12 +0900

    Update list.py

--- list.py
+++ list.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template
-import os,sys
+import os
 list_page = Blueprint('list_page', __name__)
+
 
 @list_page.route('/upload_show')
 def list_show():
-    print(os.listdir('static/images/upload'))
+    file_names = [i for i in os.listdir('./static/images/upload') if i[0] != '.']
+    
+    
     return render_template('list_page.html')
-
