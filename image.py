b2751fcb34c5f084e28bf9fae8e5be2f2272632d
piry9200 <ymt104ro09@gmail.com>
2023-12-21 15:44:10 +0900

    Update image.py

--- image.py
+++ image.py
@@ -6,12 +6,18 @@ from watchdog.events import LoggingEventHandler
 
 #LoggingEvenHandlerを上書きして動作を変更
 class LoggingEventHandler2(LoggingEventHandler):
-    def on_created(self, event):    
+    def on_created(self, event):
+        file_absolute_path = event.src_path
+        file_name = event.src_path.split("/")[-1]
         print("生成されました。" + event.src_path) #デバッグ用
-        print(type(event.src_path)) #デバッグ用
-        img = cv2.imread(event.src_path)
-        print(type(img)) #デバッグ用
+        print("生成されたファイルの名前は " + file_name + "です。") #デバッグ用
         # ---以下,画像が新たにアップロードされたときに実行する処理
+
+        #画像読み込み
+        img = cv2.imread(path)
+        #[デバッグ用]新たに読み込まれた画像を表示 ここでエラー, 「Exception in thread Thread-1: ...」 imwrite等でも同様のエラー起きる
+        cv2.imshow("uploaded_img", img)
+        cv2.waitKey(1)
         
 
     def on_deleted(self, event):
